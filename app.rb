2cc53dc4fab7e99a82627bb0037526a63166b4fa
knbrlo <kenbrlow@gmail.com>
2020-12-11 17:28:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -14,5 +14,17 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do
+    "Goodbye, #{params[:name]}."
+  end
 
+  # Example dynamic route
+  get '/multiply/:num_one/:num_two' do
+    @first_number = params[:num_one].to_i
+    @second_number = params[:num_two].to_i
+    @final_number = @first_number * @second_number
+    @final_number.to_s
+  end
+
+  
 end
